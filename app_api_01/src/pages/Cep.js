487ac0396e93c2e08af03f8eca209dcf77327682
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-07-13 09:10:43 -0300

    add novo

--- app_api_01/src/pages/Cep.js
+++ app_api_01/src/pages/Cep.js
@@ -1,9 +1,20 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
 export default props => {
+
+  const [cep, setCep] = useState('');
+
+  function buscarCep(){
+
+  }
+
   return (
     <View>
       <Text>Tela de CEP</Text>
+      <TextInput placeholder='CEP somente números' onChangeText={setCep} />
+      <Button title='Pesquisar' onPress={buscarCep} />
+      <Text>Logradouro:</Text>
     </View>
   )
 }
