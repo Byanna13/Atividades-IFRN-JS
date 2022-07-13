74bae5ba951677e80fc2fcf4ee4c93b3c29f7c8e
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-07-13 08:07:17 -0300

    add

--- app_api_01/App.js
+++ app_api_01/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Index from './src/pages/Index';
+import Cep from './src/pages/Cep';
 
-export default function App() {
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName='Index'>
+        <Tab.Screen name='Index' component={Index} />
+        <Tab.Screen name='Cep' component={Cep} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app_api_01/src/pages/Index.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default props => {
+  return (
+    <View>
+      <Text>Tela Inicial</Text>
+    </View>
+  )
+}
