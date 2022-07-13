9cf4681cb3cef9b1c0302a4e6fb18b9f61b4a45a
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-07-13 10:31:35 -0300

    okay

--- app_api_01/App.js
+++ app_api_01/App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Index from './src/pages/Index';
 import Cep from './src/pages/Cep';
+import Temperatura from './src/pages/Temperatura';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -12,6 +13,7 @@ export default () => {
       <Tab.Navigator initialRouteName='Cep'>
         <Tab.Screen name='Index' component={Index} />
         <Tab.Screen name='Cep' component={Cep} />
+        <Tab.Screen name='Temperatura' component={Temperatura} />
       </Tab.Navigator>
     </NavigationContainer>
   )
---
+++ app_api_01/src/pages/Temperatura.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import axios from 'axios';
+
+export default props => {
+
+  const [temp, setTemp] = useState('');
+  const [tempResultado, setTempResultado] = useState('');
+
+  async function buscarTemp(){
+    const resutado = await axios.get('https://goweather.herokuapp.com/weather/'+temp+'');
+    setTempResultado(resutado.data);
+  }
+
+  return (
+    <View>
+      <Text>Tela de Temperatura</Text>
+      <TextInput placeholder='Digite a Cidade' onChangeText={setTemp} />
+      <Button title='Pesquisar' onPress={buscarTemp} />
+      <Text>Temperatura: {tempResultado.temperature} </Text>
+    </View>
+  )
+}
